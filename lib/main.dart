82f43eedfbb00b80f367b850cfaacf177350620a
Den <de2.nis7.a@gmail.com>
2025-10-16 22:59:23 +0100

    ex1Flutter: Open Flutter DevTools

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,18 @@ class App extends StatelessWidget {
   const App({super.key});
 
   @override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Sandwich Shop App',
-    home: Scaffold(
-      appBar: AppBar(title: const Text('Sandwich Counter')),
-      body: const Center(
-        child: OrderItemDisplay(5, 'Footlong'),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sandwich Shop App',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Sandwich Counter')),
+        body: const Center(
+          child: OrderItemDisplay(5, 'Footlong'),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
-}
-
 
 class OrderItemDisplay extends StatelessWidget {
   final String itemType;
@@ -29,7 +28,7 @@ class OrderItemDisplay extends StatelessWidget {
   const OrderItemDisplay(this.quantity, this.itemType, {super.key});
 
   @override
-Widget build(BuildContext context) {
-  return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
-}
+  Widget build(BuildContext context) {
+    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,8 +13,8 @@ import 'package:sandwich_shop/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
+    await tester.pumpWidget(const App());
+//myapp
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
