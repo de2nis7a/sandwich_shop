2b1a28a55c5dfa914a9bc35c6ee0a1f872eaa7da
Den <de2.nis7.a@gmail.com>
2025-10-23 20:51:12 +0100

    Display three OrderItemDisplay and add horizontal scroll

--- lib/main.dart
+++ lib/main.dart
@@ -10,24 +10,70 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sandwich Shop App',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: Center(
-          child: Container(
-              width: 500,
-              height: 300,
-              color: Colors.blue,
-              child: const Center(child: OrderItemDisplay(5, 'Footlong'))),
-        ),
-      ),
-    );
+        title: 'Sandwich Shop App',
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Sandwich Counter')),
+            body: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    width: 500,
+                    height: 300,
+                    color: Colors.blue,
+                    child: const Center(child: OrderItemDisplay(3, 'BLT')),
+                  ),
+                  Container(
+                    width: 500,
+                    height: 300,
+                    color: const Color.fromARGB(255, 151, 154, 167),
+                    child: const Center(child: OrderItemDisplay(5, 'Club')),
+                  ),
+                  Container(
+                    width: 500,
+                    height: 300,
+                    color: Colors.red,
+                    child: const Center(child: OrderItemDisplay(5, 'Veggie')),
+                  ),
+                ],
+              ),
+            )));
   }
 }
+//           Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               children: [
+//                 Expanded(
+//                     child: Container(
+//                         width: 500,
+//                         height: 300,
+//                         color: Colors.blue,
+//                         child:
+//                             const Center(child: OrderItemDisplay(3, 'BLT')))),
+//                 Expanded(
+//                   child: Container(
+//                       width: 500,
+//                       height: 300,
+//                       color: Colors.blue,
+//                       child: const Center(child: OrderItemDisplay(5, 'Club'))),
+//                 ),
+//                 Expanded(
+//                   child: Container(
+//                       width: 500,
+//                       height: 300,
+//                       color: Colors.blue,
+//                       child:
+//                           const Center(child: OrderItemDisplay(5, 'Veggie'))),
+//                 ),
+//               ])),
+//     );
+//   }
+// }
 
 class OrderItemDisplay extends StatelessWidget {
-  final String itemType;
   final int quantity;
+  final String itemType;
 
   const OrderItemDisplay(this.quantity, this.itemType, {super.key});
 
